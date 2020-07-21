926558cab317d607cfb940f1ee06253688856d0a
ChaozzJ <direccion@epicalesoft.com>
2020-07-21 17:00:03 -0600

    aesthetic

--- website/redpergamino.net/resources/views/pergamino/hero.blade.php
+++ website/redpergamino.net/resources/views/pergamino/hero.blade.php
@@ -1,11 +1,6 @@
  <!-- ======= Hero Section ======= -->
  <section id="hero">
-
-<div class="container">
   <div class="row hero-map">
-    <img src="{{URL::asset('img/map.png')}}" class="img-fluid" alt="" class="hero-map">
-    
+    <img src="{{URL::asset('img/banner.png')}}" class="img-fluid" alt="" class="hero-map">
   </div>
-</div>
-
 </section><!-- End Hero -->
