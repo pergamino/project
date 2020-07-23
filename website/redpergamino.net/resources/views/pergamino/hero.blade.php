cb97a8bc74251df3b76791168c586ea20c8e5716
ChaozzJ <direccion@epicalesoft.com>
2020-07-23 17:19:10 -0600

    hero slider fix

--- website/redpergamino.net/resources/views/pergamino/hero.blade.php
+++ website/redpergamino.net/resources/views/pergamino/hero.blade.php
@@ -3,10 +3,10 @@
   <div class="flexslider">
     <ul class="slides">
       <li>
-        <img src="{{URL::asset('img/hero/2.jpg')}}" />
+        <img src="{{URL::asset('img/hero/2.jpg')}}" style="width:100% !important;" />
       </li>
       <li>
-        <img src="{{URL::asset('img/hero/3.jpg')}}" />
+        <img src="{{URL::asset('img/hero/3.jpg')}}" style="width:100% !important;"/>
       </li>
     </ul>
   </div>
