0366ab98b06b0f32efbc8a853eb88f4785695da5
eliseo.martinez@gmail.com <alejandro.solis@gmail.com>
2021-01-10 20:33:54 -0600

    para utilizar datos del modelo del clima

--- modulo ipsim/ipsim/0.1-archivo_meteo.R
+++ modulo ipsim/ipsim/0.1-archivo_meteo.R
@@ -3,8 +3,19 @@
 ################
 
 fichier_meteo_new <- function(df_meteo0){
-  
-df_meteo0$fecha0 <- as.Date(as.character(df_meteo0$fecha),format = "%d/%m/%Y")
+  if("precipitacion" %in% colnames(df_meteo0) && "tempmedia" %in% colnames(df_meteo0))
+  {
+    names(df_meteo0)[names(df_meteo0) == "precipitacion"] <- "precip"
+    names(df_meteo0)[names(df_meteo0) == "tempmedia"] <- "tmean"
+    names(df_meteo0)[names(df_meteo0) == "fecha"] <- "fecha"
+    df_meteo0$fecha <- format(strptime(as.character(df_meteo0$fecha), "%m/%d/%y" ),"%d/%m/%Y")
+    df_meteo0$fecha0 <- as.Date(as.character(df_meteo0$fecha),format = "%d/%m/%Y")
+    print(df_meteo0)
+  }
+  else {
+    df_meteo0$fecha0 <- as.Date(as.character(df_meteo0$fecha),format = "%d/%m/%Y")
+  }
+
 df_meteo0$fecha2 <- as.POSIXlt(df_meteo0$fecha0)
 # j ai change "mes" en "num_mes"
 df_meteo0$num_mes <- as.numeric(strftime(df_meteo0$fecha2,format="%m"))
