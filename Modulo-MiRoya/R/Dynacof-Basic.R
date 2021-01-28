8df49d76b437a37746fcde5fb38de6b3cb8fbd41
Pierre Bommel <bommel@cirad.fr>
2021-01-28 12:03:01 +0100

    Delete Modulo-MiRoya/R directory

--- Modulo-MiRoya/R/Dynacof-Basic.R
+++
@@ -1,8 +0,0 @@
-
-#remotes::install_github("VEZY/DynACof")
-rm(list = ls())
-library("DynACof")
-Sys.setenv(TZ="UTC")
-sim = DynACof(Period= as.POSIXct(c("1979-01-01", "1984-01-31")))
-plot(sim$Meteo$Date,sim$Sim$LAI)
-
