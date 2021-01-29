3f1d355db2b6959e441015736ead501a5fb45324
eliseo.martinez@gmail.com <alejandro.solis@gmail.com>
2021-01-29 13:07:51 -0600

    Verify true en script integracion

--- integracion/pergaminoccgt.py
+++ integracion/pergaminoccgt.py
@@ -18,7 +18,7 @@ cctoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2Fw
 pwtoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidmlnaWxhbmNpYUBwZXJnYW1pbm93ZWIuY29tIiwiZXhwIjoxNjA3OTEzMDIwLCJpc3MiOiJwZXJnYW1pbm93ZWIiLCJhdWQiOiJQZXJnYW1pbm9BcHBVc2VycyJ9.45UrFmDHyleelPDwAQT1EVra2E2C3cNo1iPGxZzK0ME"
 ccrefreshtoken = "3mX8CI4bE+JNKwNqQoBhCPiluSJ8ZZxMpNw+sSwTeF8="
 pwrefreshtoken = "8dsFx47x63xhFcaV7+Q8X/nNRoiUz1afR/BUalCoytQ="
-resultrefresh = requests.post(ccurl + "/api/authentication/refresh", verify=False, json={"OldToken":cctoken,"RefreshToken":ccrefreshtoken}, headers={"Content-Type":"application/json"})
+resultrefresh = requests.post(ccurl + "/api/authentication/refresh", verify=True, json={"OldToken":cctoken,"RefreshToken":ccrefreshtoken}, headers={"Content-Type":"application/json"})
 if resultrefresh == 200:
     ntoken = resultrefresh.text
     h = {
@@ -30,11 +30,11 @@ if resultrefresh == 200:
     elements = data[0]
     for item in elements:
         item["pais"] = "GT"
-    resultrefresh = requests.post(pwurl + "/api/authentication/refresh", verify=False, json={"OldToken":pwtoken,"RefreshToken":pwrefreshtoken}, headers={"Content-Type":"application/json"})
+    resultrefresh = requests.post(pwurl + "/api/authentication/refresh", verify=True, json={"OldToken":pwtoken,"RefreshToken":pwrefreshtoken}, headers={"Content-Type":"application/json"})
     if resultrefresh == 200:
         ntoken = resultrefresh.text
         h = {
             "Authorization": "Bearer " + ntoken,
             "Content-Type": "application/json"
         }
-        resultinsert = requests.post(pwurl + "/api/replicacion/saveVigilanciaExterna", json={"vigilancia":elements}, verify=False, headers=h)
+        resultinsert = requests.post(pwurl + "/api/replicacion/saveVigilanciaExterna", json={"vigilancia":elements}, verify=True, headers=h)
