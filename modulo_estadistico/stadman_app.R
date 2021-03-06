9c5ec8686627132f58e11c44ed331fffea441c8c
Jaime <jaimeclimax@gmail.com>
2021-03-06 11:16:16 -0600

    correcion a stadman app

--- modulo_estadistico/stadman_app.R
+++ modulo_estadistico/stadman_app.R
@@ -212,8 +212,8 @@ func_stadaman_app <- function() {
 
           dbDisconnect(con)   #disconnect from database
           
-          #return(rs_insert_cabecera$correl)
-          return(69)
+          return(rs_insert_cabecera$correl)
+          #return(69)
           
 
           
