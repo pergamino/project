a4dea941a710a8c98fb8508d11b676d05d637a58
eliseo.martinez@gmail.com <eliseo.martinez@gmail.com>
2021-03-10 20:47:51 -0600

    Creditos optiroya

--- modulo_optimizacion/optimizacion/app5.R
+++ modulo_optimizacion/optimizacion/app5.R
@@ -110,7 +110,13 @@ ui <- fluidPage(
         menuItem("Análisis Sin Datos", tabName = "SinDatos", icon = icon("th"))
       
       ),
-      tags$div(style="padding:15px;text-align:center;",actionLink("show", "Ver guía de uso", style="margin:20px;"))
+      tags$div(style="padding:15px;text-align:center;",actionLink("show", "Ver guía de uso", style="margin:20px;")),
+      tags$div(style="width: 215px;color: white;text-align: center;padding: 15px;",
+               HTML("<br><br><br><br><br><br><br><br><br><br><br><br><br><b>Créditos:</b><br>Edwin Treminio, Edwin.Treminio@catie.ac.cr <br><br>
+              Sergio Vílchez, svilchez@catie.ac.cr <br>
+              Natacha Motisi, natacha.motisi@cirad.fr <br>
+              Fernando Casanoves, casanoves@catie.ac.cr <br>
+              Jacques Avelino,  jacques.avelino@cirad.fr "))
     ),
     
     
